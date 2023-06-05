8d896a1a1b32c1391409706ced2a555c90e0f77b
Ryan Wark <ryan@brzwire.com>
2023-06-05 13:50:41 -0300

    Adding deployment support for Github pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,11 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: process.env.BASE_PATH || '',
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: "index.html"
+			fallback: "index.html",
 		})
 	}
 };
